21d0d0a0559eb89021d0262a5924c020ca6d84f3
Tirtho Sarathi Saha <t.saha@tu-braunschweig.de>
2023-11-28 00:35:52 +0100

    ode_model_update

--- src/SCIANN/SciannModel/model_ode_solver.py
+++ src/SCIANN/SciannModel/model_ode_solver.py
@@ -40,28 +40,43 @@ class BaseModel(ABC):
         pass
 
 class SaturatedGrowthModel(BaseModel):
-    def __init__(self, C):
+    def __init__(self, C, initial_conditions, tend):
         super().__init__()
         self.C = C
+        self.initial_conditions = initial_conditions
+        self.tend = tend
 
     def _model(self, u, t):
         return u*(self.C -u)
     
-    def plot_solution(self, initial_conditions, t_end):
-        t_span = np.arange(0,t_end, 0.1)
-        _, solution = self.solve_ode(initial_conditions=initial_conditions, t_span=t_span)
+    def plot_solution(self, ax=None, set_title=None):
+        t_span = np.arange(0, self.tend, 0.1)
+        _, solution = self.solve_ode(initial_conditions=self.initial_conditions, t_span=t_span)
         u = solution[:, 0]
-        plt.plot(t_span, u)
-        plt.plot(t_span, u)
-        plt.title("Saturated Growth Model")
-        plt.xlabel("Time")
-        plt.ylabel("Population")
-        plt.show()
+
+        if ax is None:
+            fig, ax = plt.subplots()
+            ax.plot(t_span, u)
+            if set_title is None:
+                ax.set_title("SG Model")
+            else:
+                ax.set_title(set_title)
+            ax.set_xlabel("Time")
+            ax.set_ylabel("Population")
+            plt.show() 
+        else:
+            ax.plot(t_span, u)
+            if set_title is not None:
+                ax.set_title(set_title)
+            ax.set_xlabel("Time")
+            ax.set_ylabel("Population")
 
 class CompetitionModel(BaseModel):
-    def __init__(self, params):
+    def __init__(self, params, initial_conditions, tend):
         super().__init__()
         self.params = params
+        self.initial_conditions = initial_conditions
+        self.tend = tend
 
     def _model(self, q, t):
         u, v = q
@@ -70,18 +85,28 @@ class CompetitionModel(BaseModel):
         dvdt = r*v*(1-b1*u-b2*v) +f0 + f1*u + f2*v + f3*u*u*v + f4*u*v*v
         return [dudt, dvdt]
     
-    def plot_solution(self, initial_conditions, t_end):
-        # initu, initv = initial_conditions
-        t_span = np.arange(0, t_end, 0.1)
-        _, solution = self.solve_ode(initial_conditions=initial_conditions, t_span=t_span)
+    def plot_solution(self, ax=None, set_title=None):
+        t_span = np.arange(0, self.tend, 0.1)
+        _, solution = self.solve_ode(initial_conditions=self.initial_conditions, t_span=t_span)
         u, v = solution[:, 0], solution[:, 1]
-        plt.plot(t_span, u, label="Population 1")
-        plt.plot(t_span, v, label="Population 2")
-        plt.title("Competition Model")
-        plt.xlabel("Time")
-        plt.ylabel("Population")
-        plt.legend()
-        plt.show()
+
+        created_fig = False
+        if ax is None:
+            fig, ax = plt.subplots()
+            created_fig = True
+
+        ax.plot(t_span, u, label="Population 1")
+        ax.plot(t_span, v, label="Population 2")
+        if set_title is None:
+            ax.set_title("Comp Model")
+        else:
+            ax.set_title(set_title)
+        ax.set_xlabel("Time")
+        ax.set_ylabel("Population")
+        ax.legend()
+
+        if created_fig:
+            return fig
 
 def create_train_data(model, tend, 
                       initial_conditions, numpoints=500, 
@@ -104,7 +129,7 @@ def create_train_data(model, tend,
     """
     # check conditions
     if tend <= 0:
-        raise ValueError("End time 'ten' must be greater than zero.")
+        raise ValueError("End time 'tend' must be greater than zero.")
     if numpoints <=1:
         raise ValueError("Numer of points 'numpoints' must be greater than one.")
     if time_limit is not None:
@@ -116,6 +141,7 @@ def create_train_data(model, tend,
     # sparsity 
     if sparse:
         tTrain = np.sort(np.random.uniform(0, tend, numpoints))
+        tTrain[0] = 0.0
     else:
         tTrain = np.linspace(0, tend, numpoints)
 
@@ -125,11 +151,14 @@ def create_train_data(model, tend,
         time_limit = [0, tend]
 
     mask = (t_span == t_span[0]) | ((t_span >= time_limit[0]) & (t_span <= time_limit[1]))
+    # mask = ((t_span >= time_limit[0]) & (t_span <= time_limit[1]))
     tTrain = t_span[mask]
     sol = sol[mask]
 
     noise = np.random.normal(0, noise_level, sol.shape)
-    sol_noisy = sol + noise
+    sol_noisy = sol.copy()
+    for col in range(sol_noisy.shape[1]):
+        sol_noisy[1:, col] += noise[1:, col]
 
     if show_figure:
         for i in range(sol_noisy.shape[1]):
@@ -137,7 +166,34 @@ def create_train_data(model, tend,
             plt.legend()
         plt.title('Training Points')
         plt.xlabel('t')
-        plt.ylabel('# of Cell')
+        plt.ylabel('Population')
         plt.show()
 
     return tTrain, sol_noisy
+
+
+def model_comparison(true_model, predicted_model):
+    t_span = np.arange(0, true_model.tend, 0.01)
+    _, true_sol = true_model.solve_ode(true_model.initial_conditions, t_span)
+    _, predicted_sol = predicted_model.solve_ode(predicted_model.initial_conditions, t_span)
+
+    fig, ax = plt.subplots()
+    
+    # Check if the solution includes both u and v
+    if true_sol.shape[1] == 2:
+        # Plot u and v for both true and predicted solutions
+        ax.plot(t_span, true_sol[:, 0], label='True u', linestyle='-', color='blue', linewidth=2)
+        ax.plot(t_span, true_sol[:, 1], label='True v', linestyle='-', color='green', linewidth=2)
+        ax.plot(t_span, predicted_sol[:, 0], label='Predicted u', linestyle='--', color='blue',linewidth=2)
+        ax.plot(t_span, predicted_sol[:, 1], label='Predicted v', linestyle='--', color='green', linewidth=2)
+    else:
+        # Plot only u for both true and predicted solutions
+        ax.plot(t_span, true_sol[:, 0], label='True Solution', linestyle='-', linewidth=2)
+        ax.plot(t_span, predicted_sol[:, 0], label='Predicted Solution', linestyle='--', linewidth=2)
+
+    ax.set_title('Model Comparison')
+    ax.set_xlabel('Time')
+    ax.set_ylabel('Population')
+    ax.legend()
+
+    return fig
