1a5b16e8f27300dd6a49ab76c877ebe08a24ac32
Tirtho Sarathi Saha <t.saha@tu-braunschweig.de>
2024-02-27 21:13:27 +0100

    get_us & get_x_batch & export_mse_mae

--- src/FBPINNs/FBPINNsModel/plot.py
+++ src/FBPINNs/FBPINNsModel/plot.py
@@ -8,6 +8,8 @@ from fbpinns.analysis import load_model
 from fbpinns.analysis import FBPINN_solution as FBPINN_solution_
 from fbpinns.analysis import PINN_solution as PINN_solution_
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 """
 i = epochs
 
@@ -44,22 +46,11 @@ def PINN_solution(c, model):
     u_test = PINN_solution_(c, all_params, x_batch)
     return u_test.reshape(c.n_test)
 
-def plot_model_comparison(run, rootdir="results/", type=None, ax=None):
+def plot_model_comparison(c, model, type=None, ax=None):
     if type not in ["FBPINN", "PINN"]:
         raise ValueError("Invalid type specified. Please use 'FBPINN' or 'PINN'.")
-    c, model = load_model(run, rootdir=rootdir)   
-    all_params, domain, problem, active = model[1], c.domain, c.problem, model[3]
-    x_batch = domain.sample_interior(all_params=all_params, key=None, sampler="grid", batch_shape=c.n_test)
-    u_exact = problem.exact_solution(all_params, x_batch, batch_shape=c.n_test)
-    if type=="FBPINN":
-        u_test = FBPINN_solution_(c, all_params, active, x_batch)
-    elif type=="PINN":
-        u_test = PINN_solution_(c, all_params, x_batch)
-    else:
-        raise ValueError("Invalid type specified. Please use 'FBPINN' or 'PINN'.")
-    
-    u_learned = problem.learned_solution(all_params, x_batch.reshape(-1))
-
+    u_exact, u_test, u_learned = get_us(c, model, type=type)
+    x_batch = get_x_batch(c, model)
     if ax is None:
         fig, ax = plt.subplots()
     ax.plot(x_batch, u_exact[:, 0], 'r-', label='u true')
@@ -75,3 +66,48 @@ def plot_model_comparison(run, rootdir="results/", type=None, ax=None):
     ax.set_title(f"{type} Models Comparison")
     if ax is None:
         plt.show()
+
+def get_x_batch(c, model):
+    """
+    Extract x_batch
+    """
+    all_params, domain= model[1], c.domain
+    x_batch = domain.sample_interior(all_params=all_params, key=None, sampler="grid", batch_shape=c.n_test)
+    return x_batch
+
+def get_us(c, model, type=None):
+    """
+    Extract u_exact, u_test, u_learned
+    using model and constants
+    """
+    if type not in ["FBPINN", "PINN"]:
+        raise ValueError("Invalid type specified. Please use 'FBPINN' or 'PINN'.")
+    all_params, domain, problem, active = model[1], c.domain, c.problem, model[3]
+    x_batch = domain.sample_interior(all_params=all_params, key=None, sampler="grid", batch_shape=c.n_test)
+    u_exact = problem.exact_solution(all_params, x_batch, batch_shape=c.n_test)
+    if type=="FBPINN":
+        u_test = FBPINN_solution_(c, all_params, active, x_batch)
+    elif type=="PINN":
+        u_test = PINN_solution_(c, all_params, x_batch)
+    else:
+        raise ValueError("Invalid type specified. Please use 'FBPINN' or 'PINN'.")
+    u_learned = problem.learned_solution(all_params, x_batch.reshape(-1))
+    return u_exact, u_test, u_learned
+
+def export_mse_mae(u_exact, u_test, u_learned, file_path=None):
+    """
+    Calculate MSE and MAE of the predicted model and learned model
+    """
+    mse_test = np.mean((u_exact - u_test)**2)
+    mse_learned = np.mean((u_exact - u_learned)**2)
+    mae_test = np.mean(np.abs(u_exact - u_test))
+    mae_learned = np.mean(np.abs(u_exact - u_learned))
+    metrics_df = pd.DataFrame({
+        'Metric': ['MSE', 'MAE'],
+        'Test': [mse_test, mae_test],
+        'Learned': [mse_learned, mae_learned]
+    })
+    if file_path is not None:
+        metrics_df.to_csv(file_path, index=False)
+    else:
+        metrics_df.to_csv('metrics.csv', index=False)
