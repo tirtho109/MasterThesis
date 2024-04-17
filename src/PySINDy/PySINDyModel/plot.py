0fd3345a93c3b4f97cd5ab75f43f168e918bff5d
Tirtho Sarathi Saha <t.saha@tu-braunschweig.de>
2024-04-17 13:06:28 +0200

    added plot_ensemble_results(), plot_data_and_derivative()
    plot ensemble result shows the model featutes with mean and std for normal ensemble and library ensemble method, and plot_data_and_derivative() shows the given data and it's derivative to see how much is the fluctuation in case of high noise in the data, also has the functionality to use FiniteDifference() and SmoothFiniteDifference() to see the difference between derivative after smoothing

--- src/PySINDy/PySINDyModel/plot.py
+++ src/PySINDy/PySINDyModel/plot.py
@@ -77,3 +77,87 @@ def plot_pareto(coefs, opt, model, threshold_scan, x_test, t_test, ax1=None, ax2
     lambda_min_mse_sim = threshold_scan[min_mse_sim_index]
 
     return lambda_min_mse, lambda_min_mse_sim
+
+def plot_data_and_derivative(x, dt, deriv, num_features = 1):
+    if num_features == 1:
+        feature_name = ["x",]
+    elif num_features == 2:
+        feature_name = ["x", "y",]
+    elif num_features == 3:
+        feature_name = ["x", "y", "z"]
+    else:
+        raise ValueError("Number of features must be 1, 2, or 3.")
+    plt.figure(figsize=(20, 5))
+    for i in range(num_features):
+        plt.subplot(1, num_features, i + 1)
+        plt.plot(x[:, i], label=feature_name[i])
+        plt.grid(True)
+        plt.xlabel("t", fontsize=24)
+        plt.xticks(fontsize=18)
+        plt.yticks(fontsize=18)
+        plt.legend(fontsize=24)
+    x_dot = deriv(x, t=dt)
+    plt.figure(figsize=(20, 5))
+    for i in range(num_features):
+        plt.subplot(1, num_features, i + 1)
+        plt.plot(x_dot[:, i], label=r"$\dot{" + feature_name[i] + "}$")
+        plt.grid(True)
+        plt.xlabel("t", fontsize=24)
+        plt.xticks(fontsize=18)
+        plt.yticks(fontsize=18)
+        plt.legend(fontsize=24)
+
+# Make an errorbar coefficient plot from the results of ensembling
+def plot_ensemble_results(
+    model, mean_ensemble, std_ensemble, mean_library_ensemble, std_library_ensemble
+    ):
+    
+    # get n_features and n_equations
+    n_features = len(model.get_feature_names())
+    n_equations = len(model.feature_names)
+    # Plot results
+    xticknames = model.get_feature_names()
+    for i in range(n_features): #  --> number of features
+        xticknames[i] = "$" + xticknames[i] + "$"
+    plt.figure(figsize=(18, 4))
+    colors = ["b", "r", "k"]
+    plt.subplot(1, 2, 1)
+    plt.xlabel("Candidate terms", fontsize=22)
+    plt.ylabel("Coefficient values", fontsize=22)
+    # set title to  "Ensemble"
+    plt.title("Ensemble", fontsize=22)
+    for i in range(n_equations): # --> number of equations
+        plt.errorbar(
+            range(n_features), #  --> number of features
+            mean_ensemble[i, :],
+            yerr=std_ensemble[i, :],
+            fmt="o",
+            color=colors[i],
+            label=r"Equation for $\dot{" + model.feature_names[i] + r"}$",
+        )
+    ax = plt.gca()
+    plt.grid(True)
+    ax.set_xticks(range(n_features)) # --> number of features
+    plt.xticks(fontsize=20)
+    plt.yticks(fontsize=20)
+    ax.set_xticklabels(xticknames, verticalalignment="top")
+    plt.subplot(1, 2, 2)
+    plt.xlabel("Candidate terms", fontsize=22)
+    # set title to "Library Ensemble"
+    plt.title("Library Ensemble", fontsize=22)
+    for i in range(n_equations): # --> number of equations
+        plt.errorbar(
+            range(n_features), # --> number of features
+            mean_library_ensemble[i, :],
+            yerr=std_library_ensemble[i, :],
+            fmt="o",
+            color=colors[i],
+            label=r"Equation for $\dot{" +model.feature_names[i] + r"}$",
+        )
+    ax = plt.gca()
+    plt.grid(True)
+    plt.xticks(fontsize=20)
+    plt.yticks(fontsize=20)
+    plt.legend(fontsize=16, loc="upper right")
+    ax.set_xticks(range(n_features)) # --> number of features
+    ax.set_xticklabels(xticknames, verticalalignment="top")
